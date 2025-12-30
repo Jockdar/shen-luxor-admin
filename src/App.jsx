dd3ef2d1df4173ad7ba73dca5dd1e6489a2c7d11
Noureddine Khender <taha01fes@gmail.com>
2025-12-31 00:11:59 +0100

    Refactor App component for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -9,61 +9,39 @@ import Login from './components/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// --- LIVE BACKEND URL ---
 export const backendUrl = "https://shen-luxor-backend.vercel.app";
-// ------------------------
-
 export const currency = '$';
 
 const App = () => {
-        const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
 
-        useEffect(() => {
-            localStorage.setItem('token', token);
-        }, [token]);
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  }, [token]);
 
-        return ( <
-                div className = "bg-gray-50 min-h-screen" >
-                <
-                ToastContainer / > {
-                    token === '' ? ( <
-                        Login setToken = { setToken }
-                        />
-                    ) : ( <
-                        >
-                        <
-                        NavBar setToken = { setToken }
-                        /> <
-                        hr / >
-                        <
-                        div className = "flex w-full" >
-                        <
-                        Sidebar / >
-                        <
-                        div className = "flex-1 mx-8 my-8 text-gray-700 text-base" >
-                        <
-                        Routes >
-                        <
-                        Route path = "/add"
-                        element = { < Add token = { token }
-                            />} / >
-                            <
-                            Route path = "/list"
-                            element = { < List token = { token }
-                                />} / >
-                                <
-                                Route path = "/order"
-                                element = { < Orders token = { token }
-                                    />} / >
-                                    <
-                                    /Routes> <
-                                    /div> <
-                                    /div> <
-                                    />
-                                )
-                            } <
-                            /div>
-                        );
-                    };
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <ToastContainer />
+      {token === '' ? (
+        <Login setToken={setToken} />
+      ) : (
+        <>
+          <NavBar setToken={setToken} />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="flex-1 mx-8 my-8 text-gray-700 text-base">
+              <Routes>
+                <Route path="/add" element={<Add token={token} />} />
+                <Route path="/list" element={<List token={token} />} />
+                <Route path="/order" element={<Orders token={token} />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-                    export default App;
+export default App;
